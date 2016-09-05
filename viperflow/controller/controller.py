e28991878a1597da97f4a67575125a04b49cdc60
gaopl <gaopl@jiedaibao.com>
2016-09-05 21:15:27 +0800

    格式化文件

--- viperflow/controller/controller.py
+++ viperflow/controller/controller.py
@@ -8,17 +8,18 @@ from neutron.extensions import providernet
 
 LOG = log.getLogger(__name__)
 
+
 class Controller(object):
     def __init__(self):
         self.conn = config.getconnection()
         self.timeout = config.getconntimeout()
 
         self._check_physicalnetwork_config()
-        
+
     def _check_physicalnetwork_config(self):
         providermap = config.getprovidermap()
         tenantmap = config.gettenantmap()
-        
+
         # this used to record providernetwork:physicalnetworkid
         self.providermap = {}
         # this used to record tenant network type:physicalnetworkid
@@ -28,16 +29,16 @@ class Controller(object):
             m = item.partition(':')
             if m[1] and m[2]:
                 self.providermap[m[0]] = m[2]
-        
+
         for item in tenantmap.split(','):
             m = item.partition(':')
             if m[0] and m[1]:
                 self.tenantmap[m[0]] = m[2]
 
-        # list all physicalnetwork , assert physicalnetwork id in controller
+                # list all physicalnetwork , assert physicalnetwork id in controller
 
-    def createlogicalnetwork(self,physicalnetwork,physicalnetworktype,segid,network):
-        
+    def createlogicalnetwork(self, physicalnetwork, physicalnetworktype, segid, network):
+
         n = dict()
         if physicalnetwork:
             physicalnetworkid = self.providermap[physicalnetwork]
@@ -50,194 +51,195 @@ class Controller(object):
         n['admin_state_up'] = network['admin_state_up']
         n['mtu'] = network['mtu']
         n['id'] = network['id']
-        
+
         if physicalnetworktype == 'vlan':
             n['vlanid'] = segid
         else:
             n['vni'] = segid
 
-
         # viperflow only support one subnet, so get first one
         if network['subnets']:
             n['subnet'] = network.get('subnets')[0]
-        
-        LOG.info(" --- createlogicalnetwork -- %r",n)
+
+        LOG.info(" --- createlogicalnetwork -- %r", n)
         param = urllib.urlencode(n)
-        LOG.info(" -- param -- %r",param)
+        LOG.info(" -- param -- %r", param)
         url = self.conn + "/viperflow/createlogicalnetwork?%s" % param
 
-        urllib2.urlopen(url,timeout=self.timeout).read()
-        
-    def deletelogicalnetwork(self,networkid):
-        
-        param = urllib.urlencode({"id":networkid})
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def deletelogicalnetwork(self, networkid):
+
+        param = urllib.urlencode({"id": networkid})
         url = self.conn + "/viperflow/deletelogicalnetwork?%s" % param
 
-        urllib2.urlopen(url,timeout=self.timeout).read()
-        
-    def updatelogicalnetwork(self,item):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def updatelogicalnetwork(self, item):
         LOG.info("---- updatelogicalnetwork ---")
         param = urllib.urlencode(item)
         url = self.conn + "/viperflow/updatelogicalnetwork?%s" % param
 
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    def createlogicalport(self,port):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def createlogicalport(self, port):
         LOG.info("---- createlogicalport ----")
         p = dict()
-        
+
         p['id'] = port['id']
         p['logicalnetwork'] = port['network_id']
         p['name'] = port['name']
         p['mac_address'] = port['mac_address']
-        
-		# create router with subnet , it will createlogicalport first ipaddress == gateway
-		# viperflow will auto add this router port, so return here
-		if port['device_owner'] == "network:router_interface":
-			return
-		
+
+        # create router with subnet , it will createlogicalport first ipaddress == gateway
+        # viperflow will auto add this router port, so return here
+        if port['device_owner'] == "network:router_interface":
+            return
+
         if port['fixed_ips']:
             p['subnet'] = port['fixed_ips'][0]['subnet_id']
             p['ip_address'] = port['fixed_ips'][0]['ip_address']
-            
+
             # when create dhcp port , we should add static route
             # to metadata server on dhcp server
             if port['device_owner'] == "network:dhcp":
-                
-                staticroute = [config.getmetadataaddress(),p['ip_address']]
+
+                staticroute = [config.getmetadataaddress(), p['ip_address']]
 
                 # first get subnet routes info, add all
-                subnets = self.listsubnet(id = p['subnet'])
-                
+                subnets = self.listsubnet(id=p['subnet'])
+
                 # {subnetid: routes,subnetid2:routes}
                 routes = utils.get_host_routes_from_subnets(subnets)[p['subnet']]
-                
+
                 if staticroute not in routes:
                     routes.append(staticroute)
-                    
+
                     s = dict()
                     s['id'] = p['subnet']
                     s['host_routes'] = '`' + str(routes) + '`'
-                    
-                    self._updatesubnet(**s)
 
+                    self._updatesubnet(**s)
 
         param = urllib.urlencode(p)
         url = self.conn + "/viperflow/createlogicalport?%s" % param
 
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    def updatelogicalport(self,port):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def updatelogicalport(self, port):
         LOG.info("---- updatelogicalport ---")
-        
+
         p = dict()
-        
+
         p['id'] = port['id']
         p['name'] = port['name']
         p['mac_address'] = port['mac_address']
-		
-		if port['device_owner'] == "network:router_interface":
-			return
-			
+
+        if port['device_owner'] == "network:router_interface":
+            return
+
         param = urllib.urlencode(p)
         url = self.conn + "/viperflow/updatelogicalport?%s" % param
 
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    def deletelogicalport(self,port):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def deletelogicalport(self, port):
         LOG.info("---- deletelogicalport ---")
         portid = port['id']
-		
-		if port['device_owner'] == "network:router_interface":
-			return
-		
+
+        if port['device_owner'] == "network:router_interface":
+            return
+
         if port['fixed_ips']:
-            #p['subnet'] = port['fixed_ips'][0]['subnet_id']
+            # p['subnet'] = port['fixed_ips'][0]['subnet_id']
             subnetid = port['fixed_ips'][0]['subnet_id']
-            #p['ip_address'] = port['fixed_ips'][0]['ip_address']
+            # p['ip_address'] = port['fixed_ips'][0]['ip_address']
             ipaddress = port['fixed_ips'][0]['ip_address']
-            
+
             # when delete dhcp port , we should remove static route
             # to metadata server on dhcp server
             if port['device_owner'] == "network:dhcp":
-                
-                staticroute = [config.getmetadataaddress(),ipaddress]
+
+                staticroute = [config.getmetadataaddress(), ipaddress]
 
                 # first get subnet routes info, add all
-                subnets = self.listsubnet(id = subnetid)
-                
+                subnets = self.listsubnet(id=subnetid)
+
                 # {subnetid: routes,subnetid2:routes}
                 routes = utils.get_host_routes_from_subnets(subnets)[subnetid]
-                
+
                 if staticroute in routes:
                     routes.remove(staticroute)
-                    
+
                     s = dict()
                     s['id'] = subnetid
                     s['host_routes'] = '`' + str(routes) + '`'
-                    
+
                     self._updatesubnet(**s)
 
-        param = urllib.urlencode({"id":portid})
+        param = urllib.urlencode({"id": portid})
         url = self.conn + "/viperflow/deletelogicalport?%s" % param
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    
-    def createsubnet(self,subnet):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def createsubnet(self, subnet):
 
         s = dict()
 
         s['id'] = subnet['id']
         s['name'] = subnet['name']
         s['logicalnetwork'] = subnet['network_id']
-        
+
         s['cidr'] = subnet['cidr']
-        #openstack have multi allocated segment ip ,
-        #now controller support only one segment,
-        #so get first segment from openstack
+        # openstack have multi allocated segment ip ,
+        # now controller support only one segment,
+        # so get first segment from openstack
         s['allocated_start'] = subnet['allocation_pools'][0]['start']
         s['allocated_end'] = subnet['allocation_pools'][0]['end']
-        
+
         s['gateway'] = subnet['gateway_ip']
         s['enable_dhcp'] = subnet['enable_dhcp']
-        
+
         s['dns_nameservers'] = subnet['dns_nameservers']
 
         routes = []
         for item in subnet['host_routes']:
             destination = item['destination']
             nexthop = item['nexthop']
-            
+
             #
             # not allow add metadata address static route  
             #
             if destination == config.getmetadataaddress():
                 continue
-            
-            routes.append([destination,nexthop])
-        
+
+            routes.append([destination, nexthop])
+
         s['host_routes'] = '`' + str(routes) + '`'
 
         param = urllib.urlencode(s)
         url = self.conn + "/viperflow/createsubnet?%s" % param
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    
-    def updatesubnet(self,subnet,origin_subnet):
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def updatesubnet(self, subnet, origin_subnet):
 
         s = dict()
 
         s['id'] = subnet['id']
         s['name'] = subnet['name']
         s['logicalnetwork'] = subnet['network_id']
-        
+
         # subnet is not allowd update cidr
-        #s['cidr'] = subnet['cidr']
-        
-        #openstack have multi allocated segment ip ,
-        #now controller support only one segment,
-        #so get first segment from openstack
+        # s['cidr'] = subnet['cidr']
+
+        # openstack have multi allocated segment ip ,
+        # now controller support only one segment,
+        # so get first segment from openstack
         s['allocated_start'] = subnet['allocation_pools'][0]['start']
         s['allocated_end'] = subnet['allocation_pools'][0]['end']
-        
+
         s['gateway'] = subnet['gateway_ip']
         s['enable_dhcp'] = subnet['enable_dhcp']
-        
+
         s['dns_nameservers'] = subnet['dns_nameservers']
 
         #
@@ -245,7 +247,7 @@ class Controller(object):
         #   add dhcp metadata static routes
         #
         if origin_subnet['host_routes'] != subnet['host_routes']:
-            
+
             newroutes = []
             for item in subnet['host_routes']:
                 destination = item["destination"]
@@ -253,36 +255,35 @@ class Controller(object):
 
                 if destination == config.getmetadataaddress():
                     continue
-                
-                newroutes.append([destination,nexthop])
-            
-            
+
+                newroutes.append([destination, nexthop])
+
             # find metadata static route , append to newroute
-            subnets = self.listsubnet(id = s['id']) 
+            subnets = self.listsubnet(id=s['id'])
             routes = utils.get_host_routes_from_subnets(subnets)[s['id']]
-            
-            for des,nh in routes:
+
+            for des, nh in routes:
                 if des == config.getmetadataaddress():
-                    newroutes.append([des,nh])
-            
+                    newroutes.append([des, nh])
+
             s['host_routes'] = '`' + str(newroutes) + '`'
-             
+
         self._updatesubnet(**s)
-    
-    def _updatesubnet(self,**kwargs):
-        
+
+    def _updatesubnet(self, **kwargs):
+
         param = urllib.urlencode(kwargs)
         url = self.conn + "/viperflow/updatesubnet?%s" % param
-        urllib2.urlopen(url,timeout=self.timeout).read()
+        urllib2.urlopen(url, timeout=self.timeout).read()
 
-    def deletesubnet(self,subnetid):
-        
-        param = urllib.urlencode({"id":subnetid})
+    def deletesubnet(self, subnetid):
+
+        param = urllib.urlencode({"id": subnetid})
         url = self.conn + "/viperflow/deletesubnet?%s" % param
-        urllib2.urlopen(url,timeout=self.timeout).read()
-    
-    def listsubnet(self,**kwargs):
-        
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def listsubnet(self, **kwargs):
+
         s = dict()
         s.update(kwargs)
 
@@ -290,73 +291,73 @@ class Controller(object):
 
         url = self.conn + "/viperflow/listsubnets?%s" % param
 
-        f = urllib2.urlopen(url,timeout=self.timeout)
-        
+        f = urllib2.urlopen(url, timeout=self.timeout)
+
         subnets = f.read()
 
         f.close()
-        
+
         return subnets
-	
-	def createvirtualrouter(self,router):
-		
-		r = dict()
-		
-		r['id'] = router['id']
-		r['name'] = router['name']
-		r['routes'] = '`' + str(router['routes']) + '`'
-		
-		param = urllib.urlencode(r)
-		
-		url = self.conn + "/vrouterapi/createvirtualrouter?%s" % param
-		
-		urllib2.urlopen(url,timeout=self.timeout).read()
-	
-	def deletevirtualrouter(self,routerid):
-		
-		r = dict()
-		
-		r['id'] = router['id']
-		
-		param = urllib.urlencode(r)
-		
-		url = self.conn + "/vrouterapi/deletevirtualrouter?%s" % param
-		
-		urllib2.urlopen(url,timeout=self.timeout).read()
-		
-	def updatevirtualrouter(self,id,router):
-		
-		r = dict()
-		
-		r['id'] = router['id']
-		
-		param = urllib.urlencode(r)
-		
-		url = self.conn + "/vrouterapi/updatevirtualrouter?%s" % param
-		
-		#urllib2.urlopen(url,timeout = self.timeout).read()
-	
-	def addrouterinterface(self,id,subnet,**kwargs):
-		
-		r = dict()
-		
-		r["router"] = id
-		r["subnet"] = subnet
-		
-		param = urllib.urlencode(r)
-		
-		url = self.conn + "/vrouterapi/addrouterinterface?%s" % param
-		
-		urllib2.urlopen(url,timeout = self.timeout).read()
-	
-	def removerouterinterface(self,id):
-		
-		r = dict()
-		
-		r["id"] = id
-		
-		param = urllib.urlencode(r)
-		
-		url = self.conn + "/vrouterapi/removerouterinterface?%s" % param
-		
-		urllib2.urlopen(url,timeout = self.timeout).read()
+
+    def createvirtualrouter(self, router):
+
+        r = dict()
+
+        r['id'] = router['id']
+        r['name'] = router['name']
+        r['routes'] = '`' + str(router['routes']) + '`'
+
+        param = urllib.urlencode(r)
+
+        url = self.conn + "/vrouterapi/createvirtualrouter?%s" % param
+
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def deletevirtualrouter(self, routerid):
+
+        r = dict()
+
+        r['id'] = router['id']
+
+        param = urllib.urlencode(r)
+
+        url = self.conn + "/vrouterapi/deletevirtualrouter?%s" % param
+
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def updatevirtualrouter(self, id, router):
+
+        r = dict()
+
+        r['id'] = router['id']
+
+        param = urllib.urlencode(r)
+
+        url = self.conn + "/vrouterapi/updatevirtualrouter?%s" % param
+
+        # urllib2.urlopen(url,timeout = self.timeout).read()
+
+    def addrouterinterface(self, id, subnet, **kwargs):
+
+        r = dict()
+
+        r["router"] = id
+        r["subnet"] = subnet
+
+        param = urllib.urlencode(r)
+
+        url = self.conn + "/vrouterapi/addrouterinterface?%s" % param
+
+        urllib2.urlopen(url, timeout=self.timeout).read()
+
+    def removerouterinterface(self, id):
+
+        r = dict()
+
+        r["id"] = id
+
+        param = urllib.urlencode(r)
+
+        url = self.conn + "/vrouterapi/removerouterinterface?%s" % param
+
+        urllib2.urlopen(url, timeout=self.timeout).read()
--- viperflow/l3/router_driver.py
+++ viperflow/l3/router_driver.py
@@ -8,88 +8,87 @@ from viperflow.controller import controller
 
 Log = log.getLogger(__name__)
 
+
 class ViperflowL3RouterPlugin(service_base.ServicePluginBase,
-								common_db_mixin.CommonDBMixin,
-								extraroute_db.ExtraRoute_dbonly_mixin):
-	
-	supported_extension_aliases = ["router","extrarouter"]
-	
-	def __init__(self):
-		Log.info(" ----- start viperflow l3 plugin -----")
-		super(ViperflowL3RouterPlugin,self).__init__()
-		self.controller = controller.Controller()
-		
-	def get_plugin_type(self):
-		return constants.L3_ROUTER_NAT
-		
-	def get_plugin_description(self):
-		return "L3 Router Service Plugin for basic L3 forwarding using viperflow"
-		
-	def create_router(self,context,router):
-		Log.info(" ---- create_router -- %r",router)
-		router = super(ViperflowL3RouterPlugin,self).
-					create_router(context,router)
-		
-		self.controller.createvirtualrouter(router)
-		
-		return router
-	
-	def delete_router(self,context,id):
-		
-		Log.info("--- delete_router -- %r",id)
-		router = super(ViperflowL3RouterPlugin,self).delete_router(context,id)
-		
-		self.controller.deletevirtualrouter(id)
-		
-		return router
-	
-	def update_router(self,context,id,router):
-		
-		original_router = self.get_router(context,id)
-		new_router = super(ViperflowL3RouterPlugin,self).update_router(
-						context,id,router)
-		Log.info("----- update_router ---- original_router %r",original_router)
-		Log.info("----- update_router ---- new_router %r",new_router)
-		
-		self.controller.updatevirtualrouter(id,router)
-		
-		return new_router
-		
-	def add_router_interface(self,context,router_id,interface_info):
-		Log.info("---- add router id %r---- ",router_id)
-		Log.info("---- add router interface_info %r",interface_info)
-		
-		rif = super(ViperflowL3RouterPlugin,self).add_router_interface(
-					context,router_id,interface_info)
-		
-		info = dict()
-		
-		# special one port as router port, in viperflow delete this port, 
-		# special this port ipaddress as router port ipaddress
-		# when remove this router port , we create this port in viperflow
-		if "port_id" in interface_info:
-			pass
-		
-		if "subnet_id" in interface_info:
-			info['subnet'] = interface_info["subnet_id"]
-		
-		self.controller.addrouterinterface(router_id,**info)
-		return rif
-	
-	def remove_router_interface(self,context,router_id,interface_info):
-		Log.info("---- remove router id %r---- ",router_id)
-		Log.info("---- remove router interface_info %r",interface_info)
-		
-		rif = super(ViperflowL3RouterPlugin,self).remove_router_interface(
-					context,router_id,interface_info)
-		
-		info = dict()
-		
-		if "port_id" in interface_info:
-			pass
-		
-		if "subnet_id" in interface_info:
-			info["subnet"] = interface_info["subnet_id"]
-		
-		self.controller.removerouterinterface(router_id,**info)
-		return rif
+                              common_db_mixin.CommonDBMixin,
+                              extraroute_db.ExtraRoute_dbonly_mixin):
+    supported_extension_aliases = ["router", "extrarouter"]
+
+    def __init__(self):
+        Log.info(" ----- start viperflow l3 plugin -----")
+        super(ViperflowL3RouterPlugin, self).__init__()
+        self.controller = controller.Controller()
+
+    def get_plugin_type(self):
+        return constants.L3_ROUTER_NAT
+
+    def get_plugin_description(self):
+        return "L3 Router Service Plugin for basic L3 forwarding using viperflow"
+
+    def create_router(self, context, router):
+        Log.info(" ---- create_router -- %r", router)
+        router = super(ViperflowL3RouterPlugin, self).create_router(context, router)
+
+        self.controller.createvirtualrouter(router)
+
+        return router
+
+    def delete_router(self, context, id):
+
+        Log.info("--- delete_router -- %r", id)
+        router = super(ViperflowL3RouterPlugin, self).delete_router(context, id)
+
+        self.controller.deletevirtualrouter(id)
+
+        return router
+
+    def update_router(self, context, id, router):
+
+        original_router = self.get_router(context, id)
+        new_router = super(ViperflowL3RouterPlugin, self).update_router(
+            context, id, router)
+        Log.info("----- update_router ---- original_router %r", original_router)
+        Log.info("----- update_router ---- new_router %r", new_router)
+
+        self.controller.updatevirtualrouter(id, router)
+
+        return new_router
+
+    def add_router_interface(self, context, router_id, interface_info):
+        Log.info("---- add router id %r---- ", router_id)
+        Log.info("---- add router interface_info %r", interface_info)
+
+        rif = super(ViperflowL3RouterPlugin, self).add_router_interface(
+            context, router_id, interface_info)
+
+        info = dict()
+
+        # special one port as router port, in viperflow delete this port,
+        # special this port ipaddress as router port ipaddress
+        # when remove this router port , we create this port in viperflow
+        if "port_id" in interface_info:
+            pass
+
+        if "subnet_id" in interface_info:
+            info['subnet'] = interface_info["subnet_id"]
+
+        self.controller.addrouterinterface(router_id, **info)
+        return rif
+
+    def remove_router_interface(self, context, router_id, interface_info):
+        Log.info("---- remove router id %r---- ", router_id)
+        Log.info("---- remove router interface_info %r", interface_info)
+
+        rif = super(ViperflowL3RouterPlugin, self).remove_router_interface(
+            context, router_id, interface_info)
+
+        info = dict()
+
+        if "port_id" in interface_info:
+            pass
+
+        if "subnet_id" in interface_info:
+            info["subnet"] = interface_info["subnet_id"]
+
+        self.controller.removerouterinterface(router_id, **info)
+        return rif
